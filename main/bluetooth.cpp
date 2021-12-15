4d0d386332d09fe7f49fe3be542d55f57e49ee3d
Drugun <drugunko@gmail.com>
2021-12-15 13:53:00 +0100

    cast funkcii BLE

--- main/bluetooth.cpp
+++ main/bluetooth.cpp
@@ -10,6 +10,57 @@
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 
+
+BLEServer *pServer;
+BLEService *pService;
+BLECharacteristic *pCharacteristic;
+BLEAdvertising *pAdvertising;
+bool waiting_ble_callback = false;
+char write_data[256];
+int write_data_counter = 0;
+char parsing_data[2][256];
+
+// BLE callbacks for wifi connection
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+
+      if (value.length() > 0) {
+        //Serial.println("*********");
+        //Serial.print("New value: ");
+        for (int i = 0; i < value.length(); i++){
+          //Serial.print(value[i]);
+          write_data[write_data_counter++] = value[i];
+        }
+        
+        //Serial.println();
+        //Serial.println("*********");
+        waiting_ble_callback = true;
+      }
+    }
+};
+
+
+
+int startBLE(){
+  BLEDevice::init("MailBox");
+  pServer = BLEDevice::createServer();
+  pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+                                       
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pCharacteristic->setValue("Wait for set wifi");
+  pService->start();
+  pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+  return 1;
+}
+
+
 int firstConfig(){
 	// TODO
 	
--- main/bluetooth.h
+++ main/bluetooth.h
@@ -1,5 +1,13 @@
 
 // funkcie na komunikaciu cez bluetooth a s mobilom
 
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+extern BLEServer *pServer;
+extern BLEService *pService;
+extern BLECharacteristic *pCharacteristic;
+extern BLEAdvertising *pAdvertising;
 
 int firstConfig();
